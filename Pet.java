25bcbc71fc41e696de9852fb70a5b5f012dcbef8
vluciano12 <cluciano@stu.socsd.org>
2021-11-30 08:21:19 -0500

    Added main files.

---
+++ Pet.java
@@ -0,0 +1,11 @@
+public class Pet{
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+    private String breed;
+
+    public Pet(String n, int a, double w, String t, String b){
+        
+    }
+}
