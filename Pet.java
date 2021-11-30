8dafffb296490fe91d380250c24d06496ecdb934
vluciano12 <cluciano@stu.socsd.org>
2021-11-30 08:36:43 -0500

    Finished the class & main method.

--- Pet.java
+++ Pet.java
@@ -6,6 +6,42 @@ public class Pet{
     private String breed;
 
     public Pet(String n, int a, double w, String t, String b){
-        
+        name = n;
+        age = a;
+        weight = w;
+        type = t;
+        breed = b;
+    }
+  
+    public Pet(){
+        name = "Goldie";
+        age = 5;
+        weight = 74.96;
+        type = "Dog";
+        breed = "Golden Retriever";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+    
+    public String getType(){
+        return type;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+
+    public String toString(){
+        return "Name: "  + name + "\nAge: " + age + "\nWeight: " + weight + " lbs\nType: " + type + "\nBreed: " + breed;
     }
 }
---
+++ main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String args[]){
+        Pet goldie = new Pet();
+        Pet archie = new Pet("Archie", 2, 0.5, "Goldfish", "Cosmic");
+
+        System.out.println(goldie);
+        System.out.println(archie);
+    }
+}
